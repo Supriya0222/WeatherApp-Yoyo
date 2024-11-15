a5f18c55308837bd8897aa60577c641af49e2a71
Supriya <supriyarajkoomar@gmail.com>
2024-11-15 19:34:55 +0400

    Added configs file to store the API KEY needed for the Open Weather api calls

--- WeatherApp/WeatherApp/Constants/AppConstants.swift
+++ WeatherApp/WeatherApp/Constants/AppConstants.swift
@@ -11,21 +11,29 @@ public struct APIServiceConstant {
     // Note: Base URL and APP ID should ideally be in a git-ignored config file, where the values can also be set using environment variables (for App Center builds for example).
     // This token is for trial and will be deactivated after the code review is done.
     
-    static let apiOpenWeatherMapAppId = "ad5ca998961f5a73c2a23cf0bd2aa311"
+    //static let apiOpenWeatherMapAppId = "ad5ca998961f5a73c2a23cf0bd2aa311"
+    
+    
     static let apiOpenWeatherIconBaseUrl = "https://openweathermap.org/img/wn/"
     static let baseUrl = "https://api.openweathermap.org"
+    
     
     public enum RequestEndpoint {
         case current
         case forecast
         
         func getUrl(_ latitude: Double, _ longitude: Double) -> String {
-            switch self {
-                case .current:
-                    return "\(baseUrl)/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiOpenWeatherMapAppId)"
-                case .forecast:
-                    return "\(baseUrl)/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=\(apiOpenWeatherMapAppId)"
+            if let apiOpenWeatherMapAppId = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String {
+                let apiKey = String(format: apiOpenWeatherMapAppId.replacingOccurrences(of: "\"", with: ""))
+                
+                switch self {
+                    case .current:
+                        return "\(baseUrl)/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)"
+                    case .forecast:
+                        return "\(baseUrl)/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)"
+                }
             }
+            return ""
         }
     }
 }
